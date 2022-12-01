a78bc5455b9925fef1ee66d9c5a1f096dbb1daa3
Jacques Vrolijk <git@jacquesvrolijk.com>
2022-12-01 14:48:23 +0000

    initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed 
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xE29b4F08485963458C82b4Dea43E1A2BfceA54BC";  //found in NFT-collection/my-app/constants/index.js
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
